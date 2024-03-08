ea92d3c6869e5e8812bf847f1aa321d82ac49780
Iryna Kolhanova <96072009+IrynaKolh@users.noreply.github.com>
2024-03-08 17:04:40 -0500

    update dependencies (#1)
    * init: add needed dependencies
    
    * feat: add error handler

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
-const app = express();
-const cors = require('cors')
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 const favicon = require('express-favicon');
 const logger = require('morgan');
 
+require('express-async-errors');
+
 const mainRouter = require('./routes/mainRouter.js');
+
+
+const app = express();
 
 // middleware
 app.use(cors());
+app.use(helmet());
+app.use(xss());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
