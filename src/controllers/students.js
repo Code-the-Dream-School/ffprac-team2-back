02a9177f306b843902198625042781fa82eb646b
Iryna <iryna.kolhanova@gmail.com>
2024-03-08 17:14:49 -0500

    feat: add all CRUD operations for student

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -3,9 +3,25 @@ const Student = require("../models/Student");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
-const getStudentsProducts = async (req, res) => {};
+const getStudentsProducts = async (req, res) => {
+   const students = await Student.find().sort(
+    "createdAt"
+  );
+  res.status(StatusCodes.OK).json({ students, count: students.length });
+};
 
-const getStudentById = async (req, res) => {};
+const getStudentById = async (req, res) => {
+  const {
+    params: { id: studentId },
+  } = req;
+  const student = await Student.findOne({
+    _id: studentId,
+  });
+  if (!student) {
+    throw new NotFoundError(`No student with id: ${studentId}`);
+  }
+  res.status(StatusCodes.OK).json({ student });
+};
 
 const addStudent = async (req, res) => {
   // req.body.createdBy = req.user.userId;
@@ -13,9 +29,51 @@ const addStudent = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ student,  msg: "Student has been successfully added" });
 };
 
-const updateStudent = async (req, res) => {};
+const updateStudent = async (req, res) => {
+  try {
+    // const userId = req.user.userId;
+    const studentId = req.params.id;
+    
+    const { name, grade } = req.body;
 
-const deleteStudent = async (req, res) => {};
+    if (!name || !grade) {
+      throw new BadRequestError("Please provide all values");
+    }
+
+    const student = await Student.findOneAndUpdate(
+      { _id: studentId },
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!student) {
+      throw new NotFoundError(`No student with id: ${studentId}`);
+    }
+    res.status(StatusCodes.OK).json({ student, msg: "Student info has been successfully updated" });   
+  } catch (err) {
+    console.error("Error fetching student:", err);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message });
+  }
+};
+
+const deleteStudent = async (req, res) => {
+  try {
+    // const userId = req.user.userId;
+    const studentId = req.params.id;
+   
+    const student = await Student.findOneAndDelete(
+      { _id: studentId },    
+    );
+
+    if (!student) {
+      throw new NotFoundError(`No student with id: ${studentId}`);
+    }
+    res.status(StatusCodes.OK).json({ student, msg: "Student has been successfully deleted" });   
+  } catch (err) {
+    console.error("Error fetching student:", err);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message });
+  }
+};
 
 module.exports = {
   getStudentsProducts,
