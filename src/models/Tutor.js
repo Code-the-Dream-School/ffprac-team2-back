fb5db5c3b234ce2d02ec653917351c227e5903a8
Anna Gerhardt <annagerhardt@Annas-MBP.lan>
2024-03-16 00:05:04 -0400

    fixed more small spelling

--- src/models/Tutor.js
+++ src/models/Tutor.js
@@ -1,139 +1,108 @@
 const mongoose = require("mongoose");
 
 const TutorSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    
-    subjectId: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-    },
-    subject: {
-        type: String,
-        enum: [
-            "English",
-            "Foreign Languages",
-            "Math",
-            "Science",
-            "Social Studies",
-        ],
-    },
-    className: {
-        English: [{ // Added englishSubjects under English category
-            type: String,
-            enum: [
-                "English",
-                "ESL",
-                "Poetry",
-                "Literacy",
-                "Reading",
-                "Writing",
-                "ACT English Test Prep",
-                "ACT Reading Test Prep",
-                "SAT Writing Test Prep",
-                "ACT Writing Test Prep",
-            ],
-        }],
-        ForeignLanguages:[{
-            type: String,
-            enum: [
-                "Spanish", 
-                "French", 
-                "Chineese", 
-                "German", 
-                "Latin"
-            ]
-        }],
-        Math: {
-            type: [String],
-            enum: [
-                "Algebra",
-                "Geometry",
-                "Trigonometry",
-                "Calculus",
-                "Statistics",
-                "Pre-Calculus",
-                "SAT Math Test Prep",
-                "ACT Math Test Prep",
-            ],
-        },
-        Science: [{ // Added scienceSubjects under Science category
-            type: String,
-            enum: [
-                "Biology",
-                "Chemistry",
-                "Earth Science",
-                "Physics",
-                "Science",
-                "ACT Science Test Prep",
-            ],
-        }],
-        
-        SocialStudies: [{ // Added scienceSubjects under Social Studies category
-            type: String,
-            enum: [
-                "World History",
-                "Psychology",
-                "US Government",
-                "Social Science",
-                "US History",
-                "Political Science",
-                "Geography",
-                "European History",
-            ],
-        }],
-    },
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  subject: {
+    type: String,
+    enum: ["English", "Foreign Languages", "Math", "Science", "Social Studies"],
+  },
+  ForeignLanguages: {
+    type: String,
+    enum: ["Spanish", "French", "Chineese", "German", "Latin"],
+  },
+  MathSubject: {
+    type: [String],
+    enum: [
+      "Math",
+      "Algebra",
+      "Geometry",
+      "Trigonometry",
+      "Calculus",
+      "Statistics",
+      "Pre-Calculus",
+      "SAT Math Test Prep",
+      "ACT Math Test Prep",
+    ],
+  },
+  Science: {
+    type: String,
+    enum: [
+      "Biology",
+      "Chemistry",
+      "Earth Science",
+      "Physics",
+      "Science",
+      "ACT Science Test Prep",
+    ],
+  },
 
-    education: {
-        type: String,
+  SocialStudies: {
+    type: String,
+    enum: [
+      "World History",
+      "Psychology",
+      "US Government",
+      "Social Science",
+      "US History",
+      "Political Science",
+      "Geography",
+      "European History",
+    ],
+  },
+
+  education: {
+    type: String,
+  },
+
+  about: {
+    type: String,
+  },
+
+  grades: [
+    {
+      type: [String],
+      enum: [
+        "K",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+      ],
     },
-    
-    about: {
-        type: String,
+  ],
+
+  yearsOfExperience: {
+    type: Number,
+    min: 1,
+    max: 50,
+  },
+
+  availability: [
+    {
+      type: [String],
+      enum: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
     },
-    
-    grades:[{
-        type: [String],
-        enum: [
-            "K",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12",
-        ],
-    }],
-    
-            
-    yearsOfExperience: {
-        type: Number,
-        min: 1,
-        max: 50,
-    },
-    
-    availability: [{
-        type: [String],
-        enum: [
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday",
-        ],
-    }],
+  ],
 });
-
 
 module.exports = mongoose.model("Tutor", TutorSchema);
